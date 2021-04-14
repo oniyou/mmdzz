f3311bb6310c94f51e5d16dc963d38fbea03cdf4
Gary_C <oniyouonion@gmail.com>
2021-04-14 21:55:18 +0800

    debug

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 var exec = require('child_process').exec;
 
-// 运行mmdzz.js
-!(async () => {
-  console.log(`\n运行mmdzz.js`)
-  var child1 = exec('node ' + 'mmdzz.js', function(err, stdout, stderr) {
-    if (err) throw err;
-    console.log(stdout);
-  })
-})()
-.catch((e) => $.logErr(e))
-  .finally(() => $.done())
+var child1 = exec('node ' + 'mmdzz.js', function(err, stdout, stderr) {
+  if (err) throw err;
+  console.log(stdout);
+});
 
-// 运行smz.js
-!(async () => {
-  console.log(`\n运行smz.js`)
-  var child1 = exec('node ' + 'smz.js', function(err, stdout, stderr) {
-    if (err) throw err;
-    console.log(stdout);
-  })
-})()
-.catch((e) => $.logErr(e))
-  .finally(() => $.done())
+var child2 = exec('node ' + 'smz.js', function(err, stdout, stderr) {
+  if (err) throw err;
+  console.log(stdout);
+});
