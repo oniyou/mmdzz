e2dac5722d1018abd7caa7188b3d68e6a276606a
Gary_C <oniyouonion@gmail.com>
2021-04-14 22:00:42 +0800

    ...

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var exec = require('child_process').exec;
 var child1 = exec('node ' + 'mmdzz.js', function(err, stdout, stderr) {
   if (err) throw err;
   console.log(stdout);
-});
+})
 
 var child2 = exec('node ' + 'smz.js', function(err, stdout, stderr) {
   if (err) throw err;
   console.log(stdout);
-});
+})
